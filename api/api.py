4c01b11e81319d8d08a4079a0cb2e49b8e3f292d
John <jhroot@users.noreply.github.com>
2018-08-15 22:55:50 +0100

    added required headers to ping endpoint and test

--- api/api.py
+++ api/api.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, Response, request
 
 
 def create_app(debug=False):
+    """
+    application factory
+    :param debug: set application to debug
+    :return: application
+    """
 
     app = Flask(__name__)
     app.debug = debug
@@ -9,6 +14,15 @@ def create_app(debug=False):
     # could move to blueprint
     @app.route("/ping")
     def ping():
-        return "pong"
+        """
+        simple pingpong responder
+        :return: response with content "pong"
+        """
+        resp = Response("pong")
+        resp.headers['Cache-Control'] = "no-store, must-revalidate"
+        resp.headers['Content-Type'] = "text/plain; charset=utf-8"
+        if request.environ.get('SERVER_PROTOCOL') == "HTTP/1.0":
+            resp.headers['Expires'] = 0
+        return resp
 
     return app
--- api/tests/ping_test.py
+++ api/tests/ping_test.py
@@ -20,6 +20,11 @@ def client():
 
 
 def test_api_ping(client):
-
+    """
+    test pingpong endpoint
+    :param client: client fixture
+    """
     res = client.get('/ping')
     assert res.data == b"pong"
+    assert res.headers['Cache-Control'] == "no-store, must-revalidate"
+    assert res.headers['Content-Type'] == "text/plain; charset=utf-8"
