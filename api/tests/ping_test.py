4fa266aecf41e9546977d78f2919660a8660d50f
John <jhroot@users.noreply.github.com>
2018-08-16 12:03:31 +0100

    Add /ping route (#6)
    * change initial route to pingpong
    
    * fixed function name
    
    * first test
    
    * added required headers to ping endpoint and test
    
    * removed redundant import
    
    * reorganised Pipfile packages
    
    * consistent quotes
    
    * Pipfile.lock update

---
+++ api/tests/ping_test.py
@@ -0,0 +1,29 @@
+import pytest
+from api.api import create_app
+
+
+@pytest.fixture
+def client():
+
+    app = create_app(debug=True)
+    ctx = app.app_context()
+    ctx.push()
+
+    # eventual db init
+    client = app.test_client()
+
+    yield client
+
+    # eventual db cleanup
+    ctx.pop()
+
+
+def test_api_ping(client):
+    """
+    test pingpong endpoint
+    :param client: client fixture
+    """
+    res = client.get("/ping")
+    assert res.data == b"pong"
+    assert res.headers["Cache-Control"] == "no-store, must-revalidate"
+    assert res.headers["Content-Type"] == "text/plain; charset=utf-8"
