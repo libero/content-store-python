b4db0c3af9482237cb0cc70a1617d44102056801
John <jhroot@users.noreply.github.com>
2018-08-17 16:16:15 +0100

    remove premature repositories file

--- content_store/api/repositories.py
+++
@@ -1,9 +0,0 @@
-from content_store.api.database import db
-from content_store.api.models import ArticleVersion
-
-
-def add_article_version():
-
-    av = ArticleVersion("abc", 1, "random content")
-    db.session.add(av)
-    db.session.commit()
