d76c4a5a67cdf17776abc72677d29a085392314a
John <jhroot@users.noreply.github.com>
2018-08-21 14:11:28 +0100

    initial model implementation

---
+++ content_store/api/repositories.py
@@ -0,0 +1,24 @@
+from sqlalchemy.orm.exc import NoResultFound
+from content_store.api.database import DB
+from content_store.api.models import ArticlePart
+
+
+class ArticlePartRepository:
+
+    def add_article_part(self, av: ArticlePart) -> None:
+
+        DB.session.add(av)
+        DB.session.commit()
+
+    def delete_article_part(self, id, version, part_name) -> None:
+
+        try:
+            article_part = DB.session.query(ArticlePart).filter_by(id=id, version=version, part_name=part_name).one()
+            DB.session.delete(article_part)
+        except NoResultFound:
+            pass
+
+    def get_article_part(self, id, version, part_name) -> ArticlePart:
+
+        article_part = DB.session.query(ArticlePart).filter_by(id=id, version=version, part_name=part_name).one()
+        return article_part
---
+++ tests/article_part_test.py
@@ -0,0 +1,50 @@
+import pytest
+from sqlalchemy.orm.exc import NoResultFound
+from content_store.api.api import create_app
+from content_store.api.config import TestingConfig
+from content_store.api.models import ArticlePart
+from content_store.api.repositories import ArticlePartRepository
+from content_store.api.database import DB
+
+
+@pytest.fixture
+def app():
+
+    application = create_app(TestingConfig)
+    with application.app_context():
+        DB.drop_all()
+        DB.create_all()
+    return application
+
+
+def test_add_get_parts(app):
+
+    with app.app_context():
+
+        article_parts = ArticlePartRepository()
+
+        test_parts = (
+            ArticlePart("001", 1, "front", "Article 001 front matter content v1"),
+            ArticlePart("001", 1, "body", "Article 001 body content v1"),
+            ArticlePart("002", 1, "front", "Article 002 front matter content v1"),
+            ArticlePart("002", 1, "body", "Article 002 front matter content v1")
+        )
+        for part in test_parts:
+            article_parts.add_article_part(part)
+
+            assert part == article_parts.get_article_part(id=part.id, version=part.version, part_name=part.part_name)
+
+
+def test_delete_part(app):
+
+    with app.app_context():
+
+        article_parts = ArticlePartRepository()
+        test_part = ArticlePart("001", 1, "front", "Article 001 front matter content v1")
+        article_parts.add_article_part(test_part)
+        part = article_parts.get_article_part(test_part.id, test_part.version, test_part.part_name)
+        assert part == test_part
+        article_parts.delete_article_part(part.id, part.version, part.part_name)
+        with pytest.raises(NoResultFound):
+            part_again = article_parts.get_article_part(test_part.id, test_part.version, test_part.part_name)
+
