5268182e62282498a772a956fb1b4060ed9ba092
John <jhroot@users.noreply.github.com>
2018-08-21 16:17:49 +0100

    tidied for linting

--- content_store/api/repositories.py
+++ content_store/api/repositories.py
@@ -5,22 +5,30 @@ from content_store.api.models import ArticlePart
 
 class ArticlePartRepository:
 
-    def add_article_part(self, av: ArticlePart) -> None:
+    @staticmethod
+    def add_article_part(article_part: ArticlePart) -> None:
 
-        DB.session.add(av)
-        DB.session.commit()
+        DB.session.add(article_part)
 
-    def delete_article_part(self, id, version, part_name) -> None:
+    @staticmethod
+    def delete_article_part(article_id, version, part_name) -> None:
 
         try:
-            article_part = DB.session.query(ArticlePart).filter_by(id=id, version=version,
-                                                                   part_name=part_name).one()
+            article_part = DB.session.query(ArticlePart).filter_by(
+                article_id=article_id,
+                version=version,
+                part_name=part_name
+            ).one()
             DB.session.delete(article_part)
         except NoResultFound:
             pass
 
-    def get_article_part(self, id, version, part_name) -> ArticlePart:
+    @staticmethod
+    def get_article_part(article_id, version, part_name) -> ArticlePart:
 
-        article_part = DB.session.query(ArticlePart).filter_by(id=id, version=version,
-                                                               part_name=part_name).one()
+        article_part = DB.session.query(ArticlePart).filter_by(
+            article_id=article_id,
+            version=version,
+            part_name=part_name
+        ).one()
         return article_part
--- tests/article_part_test.py
+++ tests/article_part_test.py
@@ -17,7 +17,8 @@ def app():
         yield application
 
 
-def test_add_get_parts(app):
+@pytest.mark.usefixtures("app")
+def test_add_get_parts():
 
     article_parts = ArticlePartRepository()
 
@@ -30,19 +31,37 @@ def test_add_get_parts(app):
     for part in test_parts:
         article_parts.add_article_part(part)
 
-        assert part == article_parts.get_article_part(id=part.id, version=part.version,
-                                                      part_name=part.part_name)
+        assert part == article_parts.get_article_part(
+            article_id=part.article_id,
+            version=part.version,
+            part_name=part.part_name
+        )
 
 
-def test_delete_part(app):
+@pytest.mark.usefixtures("app")
+def test_delete_part():
 
-    with app.app_context():
-        article_parts = ArticlePartRepository()
-        test_part = ArticlePart("001", 1, "front", "Article 001 front matter content v1")
-        article_parts.add_article_part(test_part)
-        part = article_parts.get_article_part(test_part.id, test_part.version, test_part.part_name)
-        assert part == test_part
-        article_parts.delete_article_part(part.id, part.version, part.part_name)
-        with pytest.raises(NoResultFound):
-            part_again = article_parts.get_article_part(test_part.id, test_part.version,
-                                                        test_part.part_name)
+    article_parts = ArticlePartRepository()
+    test_part = ArticlePart("001", 1, "front", "Article 001 front matter content v1")
+    article_parts.add_article_part(test_part)
+    part = article_parts.get_article_part(
+        test_part.article_id,
+        test_part.version,
+        test_part.part_name
+    )
+
+    assert part == test_part
+
+    article_parts.delete_article_part(
+        part.article_id,
+        part.version,
+        part.part_name
+    )
+
+    with pytest.raises(NoResultFound):
+
+        article_parts.get_article_part(
+            test_part.article_id,
+            test_part.version,
+            test_part.part_name
+        )
