bdefa84dc94f68e88894b757f52c0c8d94bdb8d7
Daniel Ecer <de-code@users.noreply.github.com>
2018-08-21 16:49:59 +0100

    run checks as separate commands

--- project-tests.sh
+++ project-tests.sh
@@ -5,6 +5,6 @@ set -e
 # avoid issues with .pyc/pyo files when mounting source directory
 export PYTHONOPTIMIZE=
 
-pytest && \
-    pylint content_store tests && \
-    flake8 content_store tests
+pytest
+pylint content_store tests
+flake8 content_store tests
