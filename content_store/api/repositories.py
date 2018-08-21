93e52be1848964a6f3e28bc6068f5c1cf5ab87da
John <jhroot@users.noreply.github.com>
2018-08-21 17:31:18 +0100

    changed name db for linter

--- content_store/api/repositories.py
+++ content_store/api/repositories.py
@@ -4,29 +4,29 @@ from content_store.api.models import ArticlePart
 
 class ArticlePartRepository:
 
-    def __init__(self, db):
+    def __init__(self, database):
 
-        self.db = db
+        self.database = database
 
     def add_article_part(self, article_part: ArticlePart) -> None:
 
-        self.db.session.add(article_part)
+        self.database.session.add(article_part)
 
     def delete_article_part(self, article_id, version, part_name) -> None:
 
         try:
-            article_part = self.db.session.query(ArticlePart).filter_by(
+            article_part = self.database.session.query(ArticlePart).filter_by(
                 article_id=article_id,
                 version=version,
                 part_name=part_name
             ).one()
-            self.db.session.delete(article_part)
+            self.database.session.delete(article_part)
         except NoResultFound:
             pass
 
     def get_article_part(self, article_id, version, part_name) -> ArticlePart:
 
-        article_part = self.db.session.query(ArticlePart).filter_by(
+        article_part = self.database.session.query(ArticlePart).filter_by(
             article_id=article_id,
             version=version,
             part_name=part_name
