4bea63cfd2d3dfca1fcbea9f58ac0774c6c3d9d7
John <jhroot@users.noreply.github.com>
2018-08-23 17:13:48 +0100

    change add to add_or_update

--- content_store/api/repositories.py
+++ content_store/api/repositories.py
@@ -4,13 +4,24 @@ from content_store.api.models import ArticlePart
 
 class ArticlePartRepository:
 
-    def __init__(self, database):
+    def __init__(self, database) -> None:
 
         self.database = database
 
-    def add_article_part(self, article_part: ArticlePart) -> None:
-
-        self.database.session.add(article_part)
+    def add_or_update_article_part(self, article_part: ArticlePart) -> bool:
+        """
+        :param article_part: part to add
+        :return: True for add or false for update
+        """
+        added_not_updated = False
+        try:
+            self.get_article_part(article_part.article_id, article_part.version, article_part.part_name)
+            self.database.session.merge(article_part)
+        except NoResultFound:
+            self.database.session.add(article_part)
+            added_not_updated = True
+        self.database.session.commit()
+        return added_not_updated
 
     def delete_article_part(self, article_id, version, part_name) -> None:
 
@@ -21,6 +32,7 @@ class ArticlePartRepository:
                 part_name=part_name
             ).one()
             self.database.session.delete(article_part)
+            self.database.session.commit()
         except NoResultFound:
             pass
 
