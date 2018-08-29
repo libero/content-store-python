38ea32c41a9e0e525c39f27a008e10c33b630bb6
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-08-29 15:58:01 +0100

    Add prefix

--- .docker/smoke-tests.sh
+++ .docker/smoke-tests.sh
@@ -3,8 +3,8 @@ set -e
 
 timeout=30
 
-curl_command="curl -sS localhost:5000/ping | grep pong"
-echo "Requesting /ping..."
+curl_command="curl -sS localhost:5000/articles/ping | grep pong"
+echo "Requesting /articles/ping..."
 timeout "$timeout" bash -c "
     while : ; do
         echo 'Attempt...'
